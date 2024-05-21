56f4d8ce866f1358d21157ca30f9c7ebf01586f5
Esteban Sigala <13039@portalcedi.edu.mx>
2024-05-21 13:50:23 -0600

    interfaz

--- main.cpp
+++ main.cpp
@@ -1,22 +1,60 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std; 
 using namespace sf;
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
+    RenderWindow window(VideoMode(1200, 1200), "Shipwars");
+    Texture Playertexture;
+    if (!Playertexture.loadFromFile("nave.png"))
+    {
+        cout<<"Error al cargar la textura del jugador"<< endl;
+        return 1;
+    }
+    
+    Sprite player(Playertexture);
+    player.setPosition(400,500);
+    
+    Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("space.png"))
+    {
+        cout<<"No se ha logrado cargar el fondo"<< endl;
+        return 1;
+    }
+     backgroundTexture.setRepeated(true);
+    
+    Sprite background;
+    background.setTexture(backgroundTexture);
+    background.setTextureRect(IntRect(0,0,1200,1200));
+
+    
+      
+
+    
 
     while (window.isOpen())
     {
         Event event;
         while (window.pollEvent(event))
-        {
+        {   
             if (event.type == Event::Closed)
                 window.close();
+
+             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            player.move(-5, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            player.move(5, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            player.move(0, -5);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            player.move(0, 5);
+
+
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(background);
+        window.draw(player);
         window.display();
     }
 
